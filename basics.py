9a807ca9bfeb3a018b0f86b67772cb0d7442eb0a
Mithun <nagamithunreddy@gmail.com>
2020-05-13 14:30:20 +0530

    basics

---
+++ basics.py
@@ -0,0 +1,12 @@
+age = 5
+print("age : ", age)
+print("Type of age :", type(age))
+List = []
+List.append("geeks")
+List.insert(0,"Hello ")
+List.append("Welcome")
+for i in range(len(List)):
+	print(List[i])
+x = 5
+y =1
+print("Hello",x,y)
